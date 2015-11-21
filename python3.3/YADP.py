db6801b21c22c3a073e706a004aaceecf357d4dc
Nhoya <nhoya@insicuri.net>
2015-11-21 08:34:43 +0100

    Fixed showLicense()

--- python3.3/YADP.py
+++ python3.3/YADP.py
@@ -1,11 +1,9 @@
+#!/usr/bin/python3
 import sys
-from PyQt5.QtWidgets import (QWidget, QPushButton,
-    QHBoxLayout, QVBoxLayout, QApplication, QInputDialog)
+from PyQt5.QtWidgets import (QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication, QInputDialog, QMessageBox)
 from PyQt5.QtCore import QCoreApplication
 
-
 class YADP_GUI(QWidget):
-
     def __init__(self):
         super().__init__()
 
@@ -14,7 +12,7 @@ class YADP_GUI(QWidget):
 
     def initUI(self):
         patch_button = QPushButton("Patch")
-        exit_button = QPushButton("Exit")
+        exit_button = QPushButton("Exit") 
         license_button = QPushButton("License")
         exit_button.clicked.connect(QCoreApplication.instance().quit);
         license_button.clicked.connect(self.showLicense)
@@ -32,11 +30,17 @@ class YADP_GUI(QWidget):
         self.show()
 
     def showLicense(self):
-     # stuff here
-    # Open new form w/ License
+        QMessageBox.about(self, "YADP - License", "This program is free software: you can redistribute it and/or modify "
+                                                  "it under the terms of the GNU General Public License as published by "
+                                                  "the Free Software Foundation, either version 3 of the License, or"
+                                                  "(at your option) any later version.\n\n"
+                                                  "This program is distributed in the hope that it will be useful, "
+                                                  "but without any warranty; without even the implied warranty of "
+                                                  "merchantability or fitness for a particular purpose. See the "
+                                                  "GNU General Public License for more details.")
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     ex = YADP_GUI()
     sys.exit(app.exec_())
+
