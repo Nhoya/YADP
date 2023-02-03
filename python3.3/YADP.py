6920ccaed06e34675889f9edf7c65d141a810fb2
em-s-h <120836609+em-s-h@users.noreply.github.com>
2023-02-03 06:45:39 -0300

    Organize YADP.py
    Made the code more readable.

--- python3.3/YADP.py
+++ python3.3/YADP.py
@@ -5,15 +5,16 @@ from PyQt5.QtCore import QCoreApplication
 
 
 class YADP_GUI(QWidget):
-
+  
     def __init__(self):
         super().__init__()
         self.initUI()
 
+        
     def initUI(self):
 
         self.patch_button = QPushButton("Patch")
-    #    self.patch_button.setEnabled(False)
+        #self.patch_button.setEnabled(False)
         self.patch_button.clicked.connect(self.patchClicked)
 
         exit_button = QPushButton("Exit")
@@ -28,43 +29,52 @@ class YADP_GUI(QWidget):
 
         self.rom_path = QLineEdit(self)
         self.rom_path.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        
         self.rom_label = QLabel("Rom path")
         self.rom_label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        
         select_rom = QPushButton("Select", self)
         select_rom.setToolTip('Select <b>ROM</b> file')
         select_rom.clicked.connect(self.openRom)
+        
         self.xdelta_path = QLineEdit(self)
         self.xdelta_label = QLabel("Xdelta path")
+        
         select_xdelta = QPushButton("Select",self)
         select_xdelta.setToolTip('Select <b>xdelta</b> file')
         select_xdelta.clicked.connect(self.openXdelta)
+        
         self.progressBar = QProgressBar(self)
         self.progressBar.hide()
+        
         layout_xdelta.addWidget(self.xdelta_label)
         layout_xdelta.addWidget(self.xdelta_path)
         layout_xdelta.addWidget(select_xdelta)
+        
         layout_rom.addWidget(self.rom_label)
         layout_rom.addWidget(self.rom_path)
         layout_rom.addWidget(select_rom)
+        
         layout_upper.addLayout(layout_rom)
         layout_upper.addLayout(layout_xdelta)
+        
         layout_bottom = QHBoxLayout()
         layout_bottom.addWidget(license_button)
         layout_bottom.addWidget(self.patch_button)
         layout_bottom.addWidget(exit_button)
         layout_bottom.addWidget(self.progressBar)
 
-
-
         layout_main = QVBoxLayout()
         layout_main.addLayout(layout_upper)
         layout_main.addLayout(layout_bottom)
+        
         self.setLayout(layout_main)
         self.setMaximumHeight(150)
         self.setGeometry(400, 400, 400, 150)
         self.setWindowTitle('YADP')
         self.show()
 
+        
     def showLicense(self):
         QMessageBox.about(self, "YADP - License", "This program is free software: you can redistribute it and/or modify "
                                                   "it under the terms of the GNU General Public License as published by "
@@ -75,12 +85,14 @@ class YADP_GUI(QWidget):
                                                   "merchantability or fitness for a particular purpose. See the "
                                                   "GNU General Public License for more details.")
 
+        
     def openRom(self):
         rom = QFileDialog.getOpenFileName(self, "Select Rom", "", "All files (*.*);;*.iso")
         if rom:
-           #print (rom[0])
+            #print (rom[0])
             self.showRom(rom[0])
 
+            
     def openXdelta(self):
         xfile = QFileDialog.getOpenFileName(self, "Select xDelta patch file","", "All files (*.*);;*.xdelta")
         if xfile:
@@ -88,9 +100,11 @@ class YADP_GUI(QWidget):
             self.showXdelta(xfile[0])
             self.showXdeltaFileDesc(xfile[0])
 
+            
     def showXdelta(self, path):
         self.xdelta_path.setText(path)
 
+        
     def showRom(self, rom):
         self.rom_path.setText(rom)
 
@@ -100,14 +114,14 @@ class YADP_GUI(QWidget):
         self.progressBar.show()
         self.patch_button.setEnabled(False)
 
-
-        rom = self.rom_path.text()
         xdelta_file = self.xdelta_path.text()
+        rom = self.rom_path.text()
 
         if rom and xdelta_file:
             self.progressBar.show()
-            print (rom)
             print (xdelta_file)
+            print (rom)
+            
         else:
             self.progressBar.hide()
             conf = QMessageBox.question(self, 'Error',
@@ -116,16 +130,18 @@ class YADP_GUI(QWidget):
             if conf == QMessageBox.Ok:
                 self.patch_button.setEnabled(True)
 
+                
     def showXdeltaFileDesc(self, xfile):
-        f = open(xfile, 'rb')
-        with f:
+        file = open(xfile, 'rb')
+        
+        with file:
             byte = bytearray()
             length = 0
 
-            f.seek(5)
+            file.seek(5)
             while True:
                 size = length << 7
-                byte = f.read(1)
+                byte = file.read(1)
                 length = size | (byte[0] & 0x7F)
 
                 if (byte[0] & 0x80) == 0:
@@ -134,8 +150,8 @@ class YADP_GUI(QWidget):
             if length == 0:
                 return
 
-            tmp = f.read(length)
-            f.close()
+            tmp = file.read(length)
+            file.close()
 
             if int(tmp[0]) == 94 and int(tmp[1]) == 42:         # 94 = '^', 42 = '*'
                 desc = tmp[2:]
@@ -143,6 +159,7 @@ class YADP_GUI(QWidget):
                 desc = desc.decode('utf-8')
                 QMessageBox.about(self, "Description", desc)
 
+                
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = YADP_GUI()
